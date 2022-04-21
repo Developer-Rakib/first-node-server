4fc549d7a79d99db98e196a372c7812ffa634462
Developer-Rakib <mdtazulislam6011@gmail.com>
2022-04-21 21:40:57 +0600

    step added

---
+++ _step.js
@@ -0,0 +1,26 @@
+/*
+    1. create a node server within 5 steps
+    1.1 create a folder
+    1.2 npm init
+    1.3 npm i express cors mongodb
+    1.4 script-dev : nodemon index.js
+    1.5 create index.js
+
+    ----------------------------
+    2. create atlast acount
+    2.1 signup or sign in with google
+    2.2 create a cluster
+    2.3 create userdb and password
+    2.4 network access --> ip adress --> allow all
+    2.5 database > connect > code copy
+
+    ------------------------------
+    3. CRUD oparation
+    3.1 node mongodb CRUD > fundamentals
+    3.2 create async run function
+    3.3 
+
+
+
+
+ */
